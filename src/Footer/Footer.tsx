c6e60df479e111b1f0fdf422279b5df067ebaec8
Jacobo Moreno <>
2020-07-03 07:45:25 +0900

    main visuals done

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.styles.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-secondary mt-auto py-3">
+      <div className="container">
+        <span className="innerText">Footer</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
