3f11edc07f8391942a0c8aaf1f3c5ad4c525b136
Jacobo Moreno <>
2020-07-03 18:00:53 +0900

    modifying texts

--- src/Components/PageContent/Paragraphs.tsx
+++ src/Components/PageContent/Paragraphs.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { IParagraph } from "../../dataModels";
+import "./MainContent.styles.css";
 
 export interface Props {
   paragraphs: IParagraph[];
@@ -12,7 +13,13 @@ const Paragraphs: React.FC<Props> = ({ paragraphs }) => {
     <Fragment>
       {paragraphs.map((pa) => (
         <div className="my-3">
-          <p>{pa.content}</p>
+          <p className="w-100">
+            <textarea
+              className="no-border form-control no-resize"
+              defaultValue={pa.content}
+              rows={3}
+            ></textarea>
+          </p>
           <div className="text-right">
             <button type="button" className="btn btn-primary btn-sm">
               <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
