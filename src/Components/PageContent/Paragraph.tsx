f1b068fb278d679aac6ed126e952580f83b86ea1
Jacobo Moreno <>
2020-07-04 15:08:09 +0900

    functionalities done

--- src/Components/PageContent/Paragraph.tsx
+++ src/Components/PageContent/Paragraph.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
+import ReactHtml from "raw-html-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faTrashAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { IParagraph } from "../../dataModels";
 
 export interface Props {
@@ -18,6 +19,8 @@ const Paragraph: React.FC<Props> = ({
     paragraph
   );
 
+  const [editMode, setEditMode] = useState<boolean>(false);
+
   useEffect(() => {
     setCurrentParagraph(paragraph);
   }, [paragraph]);
@@ -29,33 +32,49 @@ const Paragraph: React.FC<Props> = ({
     return true;
   };
 
+  const setHtmlAndSave = () => {
+    setEditMode(false);
+    saveThisParagraph(currentParagraph.text, currentParagraph.id);
+  };
+
   return (
     <div className="my-3">
       <p className="w-100">
-        <textarea
-          className="no-border form-control no-resize"
-          value={currentParagraph.text}
-          rows={3}
-          onChange={(e) =>
-            setCurrentParagraph({
-              id: currentParagraph.id,
-              text: e.target.value,
-            })
-          }
-        ></textarea>
+        {editMode ? (
+          <textarea
+            className="form-control no-resize"
+            value={currentParagraph.text}
+            rows={5}
+            onChange={(e) =>
+              setCurrentParagraph({
+                id: currentParagraph.id,
+                text: e.target.value,
+              })
+            }
+          ></textarea>
+        ) : (
+          <ReactHtml html={`${currentParagraph.text}`}></ReactHtml>
+        )}
       </p>
 
       <div className="text-right">
-        <button
-          type="button"
-          className="btn btn-primary btn-sm mr-1"
-          disabled={contentNotSaved()}
-          onClick={() =>
-            saveThisParagraph(currentParagraph.text, currentParagraph.id)
-          }
-        >
-          <FontAwesomeIcon icon={faSave}></FontAwesomeIcon>
-        </button>
+        {editMode ? (
+          <button
+            type="button"
+            className="btn btn-primary btn-sm mr-1"
+            onClick={() => setHtmlAndSave()}
+          >
+            <FontAwesomeIcon icon={faSave}></FontAwesomeIcon>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-primary btn-sm mr-1"
+            onClick={() => setEditMode(true)}
+          >
+            <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+          </button>
+        )}
         <button
           type="button"
           className="btn btn-primary btn-sm"
